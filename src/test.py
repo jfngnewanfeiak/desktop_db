85df78879c5add2ce602b8e93402492eb20de385
desktop_home <ok220259@oks.iput.ac.jp>
2024-08-11 07:37:52 +0900

    消します

--- src/test.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-import pathlib
-
-sys.path.append('C:\Users\81802\python_ws\sensor_data_mqtt_db')
-from mqtt_interface_pub import MQTT_PUB
-from mqtt_interface_sub import MQTT_SUB
